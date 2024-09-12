b59d9f1fef17f9ea880b60049dc1994015975658
mmatteo <matteo.martin@ynov.com>
2024-09-12 10:53:53 +0200

    fix

--- main.go
+++ main.go
@@ -1,11 +1,6 @@
 package main
 
-import "fmt"
-
 func main() {
 	setclasse() // Sets the player's class and initial inventory
 	loop()      // Starts the game loop
-	fmt.Println("test")
-	fmt.Println("test")
-	fmt.Println("test")
 }
